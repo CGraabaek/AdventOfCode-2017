c0964dff360b379fdd86b1cfd1e0417db45000ba
Christian Graabaek <cgs@syspeople.dk>
2017-12-22 14:51:56 +0100

    Added day 6 and 7, no solutions though

---
+++ Python/aoc_day6.py
@@ -0,0 +1,3 @@
+print "Advent Of Code - Day 6"
+
+PUZZLEINPUT = [int(x) for x in open('data.txt', 'r').read().split("\t")]
---
+++ Python/aoc_day7.py
@@ -0,0 +1,3 @@
+print "Advent Of Code - Day 7"
+
+PUZZLEINPUT = [int(x) for x in open('data.txt', 'r').read().split("\n")]
