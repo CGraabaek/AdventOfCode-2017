c033e021c9f83c831269d49f2b28ebe9bf4940d9
Christian Graabaek <cgs@syspeople.dk>
2017-12-24 11:15:50 +0100

    finished day 4 part 2

---
+++ Python/Day 4/aoc_day4.py
@@ -0,0 +1,20 @@
+print "Advent Of Code - Day 4"
+
+PUZZLEINPUT = [i.strip().split() for i in open('input.txt', 'r').readlines()]
+
+P1 = 0
+P2 = 0
+
+for words in PUZZLEINPUT:
+
+    # If the length of the word
+    if len(set(words)) == len(words):
+        P1 += 1
+    #Create array with all the words, where each word has been sorted
+    words = [''.join(sorted(x)) for x in words]
+    # If the length of the set is equal to the words, a set only has unique values, then it's a valid pharse.
+    if len(words) == len(set(words)):
+        P2 += 1
+
+print "Part 1: " + str(P1)
+print "Part 2: " + str(P2)
